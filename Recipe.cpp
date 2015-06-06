00e5b543387d015f4807f7d92e7df2586ed3b7d1
Eiji MATSUMOTO <e.mattsan@gmail.com>
2015-06-06 11:40:28 +0900

    spec3: レシピデータをデータファイルに分離した

--- Recipe.cpp
+++ Recipe.cpp
@@ -1,12 +1,32 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
-void printRecipe()
+static const int MAX_LINE_LENGTH = 256;
+
+void printRecipe(const char* const filename)
 {
-    std::cout << "オムライス" << std::endl;
+    std::ifstream recipe_file(filename);
+    if( ! recipe_file.good())
+    {
+        return;
+    }
+
+    while(true)
+    {
+        char recipe_data[MAX_LINE_LENGTH];
+        recipe_file.getline(recipe_data, MAX_LINE_LENGTH);
+        if(recipe_file.eof())
+        {
+            break;
+        }
+        std::cout << recipe_data << std::endl;
+    }
 }
 
-int main(int, char* [])
+int main(int argc, char* argv[])
 {
-    printRecipe();
+    printRecipe(argv[1]);
     return 0;
 }
